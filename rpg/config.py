e647d82f5c21ad4c12f66515c148e83102c74439
Cookiezi727 <78487883+Cookiezi727@users.noreply.github.com>
2021-11-29 14:20:07 -0500

    Create config.py
    settings

---
+++ rpg/config.py
@@ -0,0 +1,11 @@
+WIDTH = 1024
+HEIGHT = 768
+FPS = 60
+TILESIZE = 32 # important for overworld stuff
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+LIGHTGREY = (80, 80, 80)
